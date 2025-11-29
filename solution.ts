843cf478245b649df4f0b1d9e3a0970be1c1b45a
MDSADI01 <skshsadi123@gmail.com>
2025-11-29 20:26:50 +0600

    third commit

--- solution.ts
+++ solution.ts
@@ -12,12 +12,9 @@ const formatValue = (param : string | number |boolean)=>{
 
 
 
-console.log(formatValue('hello'));
-console.log(formatValue(5));
-console.log(formatValue(false));
 
 
-//Problem 2 
+
 
 const getLength = <T>(param : T )=>{
     if(typeof param ==='string'){
@@ -28,11 +25,9 @@ const getLength = <T>(param : T )=>{
     }
 }
 
-console.log(getLength('typescript'))
-console.log(getLength([10, 20, 30, 40]))
 
 
-//Problem - 3
+
 
 class Person {
     name : string;
@@ -48,21 +43,12 @@ class Person {
 }
 
 
-const person1 = new Person('John Doe', 30);
-console.log(person1.getDetails());
-
-const person2 = new Person('Alice', 25);
-console.log(person2.getDetails());
 
 
-//Problem 4
 
 
-const books  = [
-    { title: 'Book A', rating: 4.5 },
-    { title: 'Book B', rating: 3.2 },
-    { title: 'Book C', rating: 5.0 },
-  ];
+
+
 
   type Products = {
     title: string;
@@ -72,15 +58,9 @@ const books  = [
   const filterByRating = (param : Products[]): Products[] =>{
       return param.filter((product)=>product.rating>=4);
   }
-  console.log(filterByRating(books))
+  
 
-//   problem 5
 
-const users = [
-    { id: 1, name: 'Rakib', email: 'rakib@example.com', isActive: true },
-    { id: 2, name: 'Asha', email: 'asha@example.com', isActive: false },
-    { id: 3, name: 'Rumi', email: 'rumi@example.com', isActive: true },
-  ];
 
 type Users = {
     id : number;
@@ -93,9 +73,6 @@ const filterActiveUsers = (param : Users[]) : Users[] =>{
     return param.filter((user)=>user.isActive)
 }
  
-console.log(filterActiveUsers(users));
-
-//Problem 6
 
 
 interface Book {
@@ -109,16 +86,9 @@ const printBookDetails = (param : Book)=>{
    return `Title: ${param.title}, Author: ${param.author}, Published: ${param.publishedYear}, Available:${param.isAvailable}`
 }
 
-const myBook: Book = {
-    title: 'The Great Gatsby',
-    author: 'F. Scott Fitzgerald',
-    publishedYear: 1925,
-    isAvailable: true,
-  };
-  
-console.log(printBookDetails(myBook));
 
-  //Problem 7
+  
+
 
   const getUniqueValues = <T extends string | number>(arr1:T[],arr2:T[]): T[]=>{
 
@@ -147,11 +117,9 @@ console.log(printBookDetails(myBook));
    
   }
 
-  const array1 = [1, 2, 3, 4, 5];
-const array2 = [3, 4, 5, 6, 7];
-console.log(getUniqueValues(array1, array2));
+ 
 
-//Problem-8
+
 
 type Item = {
   name:string;
@@ -170,11 +138,7 @@ const calculateTotalPrice = (products:Item[]):number=>{
     },0)
 }
 
-const products = [
-    { name: 'Pen', price: 10, quantity: 2 },
-    { name: 'Notebook', price: 25, quantity: 3, discount: 10 },
-    { name: 'Bag', price: 50, quantity: 1, discount: 20 },
-  ];
+
   
-  console.log(calculateTotalPrice(products)); // 127.5
+ 
   
